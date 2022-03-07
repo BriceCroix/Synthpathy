a34e6471cc37faf067e344d4d1d9f572c0cf1c6a
BriceCroix <brice.croix@gmail.com>
2022-03-07 22:53:53 +0100

    Changed implementation of circular buffer

--- include/CircularBuffer.hpp
+++ include/CircularBuffer.hpp
@@ -32,7 +32,7 @@ public:
 
     CircularBuffer()
     {
-        m_read_idx, m_write_idx = 0;
+        m_count, m_write_idx = 0;
     }
 
     /**
@@ -43,48 +43,94 @@ public:
      */
     inline bool is_empty() const
     {
-        return m_read_idx == m_write_idx;
+        return !m_count;
     }
 
     /**
-     * @brief Returns the number of available data. 
-     * It is better to first check is_empty() for efficiency.
+     * @brief Indicates whether the buffer is full or not.
+     * 
+     * @return true if full.
+     * @return false if not full.
+     */
+    inline bool is_full() const
+    {
+        return m_count == size;
+    }
+
+    /**
+     * @brief Returns the number of element in the buffer. 
      * @return unsigned int number of available data.
      */
-    inline unsigned int count() const
+    inline unsigned int get_count() const
     {
-        return (m_write_idx >= m_read_idx) ? (m_write_idx - m_read_idx) : (m_write_idx + size - m_read_idx);
+        return m_count;
     }
 
     /**
-     * @brief Push new data in the buffer.
-     * 
-     * @param data The data to be pushed.
+     * @brief Push element in the buffer.
+     * @param element The element to be pushed.
      */
-    inline void push(const T& data)
+    void push(const T& element)
     {
+        if(m_count < size)
+        {
+            m_data[m_write_idx++] = element;
+            if(m_write_idx == size) m_write_idx = 0;
+            m_count++;
+        }
+    }
+
+    /**
+     * @brief Push element in the buffer.
+     * This method does not check whether there is space available or not,
+     * this may result in a loss of data.
+     * @param element The element to be pushed.
+     */
+    inline void push_fast(const T& element)
+    {
+        m_data[m_write_idx++] = element;
         if(m_write_idx == size) m_write_idx = 0;
-        m_data[m_write_idx++] = data;
+        m_count++;
     }
 
     /**
-     * @brief Pop data from the buffer.
-     * There is no guard there, so check for available data with is_empty() first. 
+     * @brief Pop element from the buffer if available.
+     * Returns a default element of template class if empty.
      */
-    inline T pop()
+    T pop()
     {
-        if(m_read_idx == size) m_read_idx = 0;
-        return m_data[m_read_idx++];
+        if(m_count)
+        {
+            const unsigned int l_read_idx = get_read_idx();
+            m_count--;
+            return m_data[l_read_idx];
+        }
+        else
+        {
+            return T();
+        }
+    }
+
+    /**
+     * @brief Pop element from the buffer.
+     * There is no guard there, so check for available data with is_empty() first.
+     * A count overflow will happen if called on an empty buffer.
+     */
+    inline T pop_fast()
+    {
+        const unsigned int l_read_idx = get_read_idx();
+        m_count--;
+        return m_data[l_read_idx];
     }
 
     /**
      * @brief Read data from the buffer without popping it.
-     * 
+     * There is no guard there, so check for available data with is_empty() first.
      * @return const T& The data to be read
      */
     inline const T& read() const
     {
-        return m_data[m_read_idx];
+        return m_data[get_read_idx()];
     }
 
     /**
@@ -93,7 +139,20 @@ public:
      */
     inline void empty()
     {
-        m_read_idx = m_write_idx;
+        m_count = 0;
+    }
+
+    /**
+     * @brief Fill the buffer with given value.
+     * 
+     * @param val 
+     */
+    void fill(T val)
+    {
+        while(m_count != size)
+        {
+            push(val);
+        }
     }
 
     /**
@@ -121,9 +180,9 @@ public:
      * 
      * @return unsigned int The write index.
      */
-    inline unsigned int get_write_idx()
+    inline unsigned int get_write_idx() const
     {
-        return (m_write_idx != size) ? m_write_idx : 0;
+        return m_write_idx;
     }
 
     /**
@@ -131,9 +190,9 @@ public:
      * 
      * @return unsigned int The read index.
      */
-    inline unsigned int get_read_idx()
+    inline unsigned int get_read_idx() const
     {
-        return (m_read_idx != size) ? m_read_idx : 0;
+        return (m_write_idx >= m_count) ? (m_write_idx - m_count) : (size + m_write_idx - m_count);
     }
 
 protected:
@@ -150,10 +209,10 @@ protected:
     unsigned int m_write_idx;
 
     /**
-     * @brief Where to read the next value.
+     * @brief The number of elements in the buffer.
      * 
      */
-    unsigned int m_read_idx;
+    unsigned int m_count;
 
 };
 
