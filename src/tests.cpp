8a3dec82addc8652618b107934bda73f730c0b46
BriceCroix <brice.croix@gmail.com>
2022-03-15 00:44:50 +0100

    Updated tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -26,6 +26,7 @@
 #include "global.h"
 #include "NoteManager.h"
 #include "multiqore.h"
+#include "waveforms.h"
 
 void perform_tests()
 {
@@ -40,50 +41,46 @@ void perform_tests()
     NoteManager& note_manager = NoteManager::get_instance();
 
     unsigned int t_us;
-    unsigned int duration_us;
     unsigned int duration_ns;
     
     while(1)
     {
         multiqore_printf("\n\n==================== Synthpathy tests ====================\n\n");
 
-        duration_us = 0;
+        t_us = time_us_32();
         for(unsigned int i = 0; i < NB_TESTS; ++i)
         {
-            t_us = time_us_32();
             controls.read_buttons(10*i);
-            duration_us += time_us_32() - t_us;
         }
-        duration_ns = duration_us * 1000 / NB_TESTS;
+        t_us = time_us_32() - t_us;
+        duration_ns = t_us * 1000 / NB_TESTS;
         multiqore_printf("controls.read_buttons() : %u ns\n", duration_ns);
 
         /*----------------------------------------------------------------------------------------*/
 
-        duration_us = 0;
+        t_us = time_us_32();
         for(unsigned int i = 0; i < NB_TESTS; ++i)
         {
-            t_us = time_us_32();
             controls.process_buttons();
-            duration_us += time_us_32() - t_us;
         }
-        duration_ns = duration_us * 1000 / NB_TESTS;
+        t_us = time_us_32() - t_us;
+        duration_ns = t_us * 1000 / NB_TESTS;
         multiqore_printf("controls.process_buttons() : %u ns\n", duration_ns);
 
         /*----------------------------------------------------------------------------------------*/
 
-        duration_us = 0;
+        t_us = time_us_32();
         for(unsigned int i = 0; i < NB_TESTS; ++i)
         {
-            t_us = time_us_32();
             note_manager.update_active_notes(42);
-            duration_us += time_us_32() - t_us;
         }
-        duration_ns = duration_us * 1000 / NB_TESTS;
+        t_us = time_us_32() - t_us;
+        duration_ns = t_us * 1000 / NB_TESTS;
         multiqore_printf("note_manager.update_active_notes(...) [Empty midi queue] : %u ns\n", duration_ns);
 
         /*----------------------------------------------------------------------------------------*/
 
-        duration_us = 0;
+        t_us = time_us_32();
         for(unsigned int i = 0; i < NB_TESTS; ++i)
         {
             // Push an event to pop
@@ -93,88 +90,83 @@ void perform_tests()
                 (i/2) % 128,
                 0x7F
             ));
-            t_us = time_us_32();
+            // Pop event
             note_manager.update_active_notes(42);
-            duration_us += time_us_32() - t_us;
         }
-        duration_ns = duration_us * 1000 / NB_TESTS;
-        multiqore_printf("note_manager.update_active_notes(...) [Not empty midi queue] : %u ns\n", duration_ns);
+        t_us = time_us_32() - t_us;
+        duration_ns = t_us * 1000 / NB_TESTS;
+        multiqore_printf("note_manager.update_active_notes(...) [push + Not empty midi queue] : %u ns\n", duration_ns);
 
         /*----------------------------------------------------------------------------------------*/
 
-        duration_us = 0;
+        t_us = time_us_32();
+        ActiveNote active_note(0, 1.f, 0, 100, 100);
         for(unsigned int i = 0; i < NB_TESTS; ++i)
         {
-            ActiveNote active_note(i%128, 1.f, 0, 100, 100);
-            t_us = time_us_32();
-            active_note.get_audio_value(i%1000, TypeWaveform::square, 0.5);
-            duration_us += time_us_32() - t_us;
+            active_note.get_audio_value(i%1000, &square_wave, 0.5, 0.5);
         }
-        duration_ns = duration_us * 1000 / NB_TESTS;
+        t_us = time_us_32() - t_us;
+        duration_ns = t_us * 1000 / NB_TESTS;
         multiqore_printf("active_note.get_audio_value(...) [square wave, alive] : %u ns\n", duration_ns);
 
         /*----------------------------------------------------------------------------------------*/
 
-        duration_us = 0;
+        t_us = time_us_32();
         for(unsigned int i = 0; i < NB_TESTS; ++i)
         {
-            ActiveNote active_note(i%128, 1.f, 0, 100, 100);
-            t_us = time_us_32();
-            active_note.get_audio_value(i%1000, TypeWaveform::saw, 0.5);
-            duration_us += time_us_32() - t_us;
+            active_note.get_audio_value(i%1000, &saw_wave, 0.f, 0.5);
         }
-        duration_ns = duration_us * 1000 / NB_TESTS;
+        t_us = time_us_32() - t_us;
+        duration_ns = t_us * 1000 / NB_TESTS;
         multiqore_printf("active_note.get_audio_value(...) [saw wave, alive] : %u ns\n", duration_ns);
 
         /*----------------------------------------------------------------------------------------*/
 
-        duration_us = 0;
+        active_note.kill();
+        t_us = time_us_32();
         for(unsigned int i = 0; i < NB_TESTS; ++i)
         {
-            ActiveNote active_note;
-            active_note.kill();
-            t_us = time_us_32();
-            active_note.get_audio_value(i%1000, TypeWaveform::square, 0.5);
-            duration_us += time_us_32() - t_us;
+            active_note.get_audio_value(i%1000, &square_wave, 0.5, 0.5);
         }
-        duration_ns = duration_us * 1000 / NB_TESTS;
+        t_us = time_us_32() - t_us;
+        duration_ns = t_us * 1000 / NB_TESTS;
         multiqore_printf("active_note.get_audio_value(...) [square wave, killed] : %u ns\n", duration_ns);
 
         /*----------------------------------------------------------------------------------------*/
 
-        duration_us = 0;
-        for(unsigned int i = 0; i < NB_TESTS; ++i)
-        {
-            ActiveNote active_note;
-            active_note.kill();
-            t_us = time_us_32();
-            active_note.get_audio_value(i%1000, TypeWaveform::saw, 0.5);
-            duration_us += time_us_32() - t_us;
-        }
-        duration_ns = duration_us * 1000 / NB_TESTS;
-        multiqore_printf("active_note.get_audio_value(...) [saw wave, killed] : %u ns\n", duration_ns);
-
-        /*----------------------------------------------------------------------------------------*/
-
         constexpr unsigned int NB_ACTIVE_NOTES = 4;
-
+        // Load Notes
         for(unsigned int i = 0; i < NB_ACTIVE_NOTES; i++)
         {
             g_midi_internal_buffer.push(midi_event_note_onoff(MIDI_NOTE_ON, 0, 12*i, 0x7F));
             note_manager.update_active_notes(42+i);
         }
 
-        duration_us = 0;
+        controls.set_selected_waveform(&square_wave);
+
+        t_us = time_us_32();
         for(unsigned int i = 0; i < NB_TESTS; ++i)
         {
             
-            t_us = time_us_32();
             note_manager.get_audio(60);
-            duration_us += time_us_32() - t_us;
         }
-        duration_ns = duration_us * 1000 / NB_TESTS;
-        multiqore_printf("note_manager.get_audio(...) [Full pool] : %u ns\n", duration_ns);
+        t_us = time_us_32() - t_us;
+        duration_ns = t_us * 1000 / NB_TESTS;
+        multiqore_printf("note_manager.get_audio(...) [Full pool, square] : %u ns\n", duration_ns);
 
+        controls.set_selected_waveform(&saw_wave);
+
+        t_us = time_us_32();
+        for(unsigned int i = 0; i < NB_TESTS; ++i)
+        {
+            
+            note_manager.get_audio(60);
+        }
+        t_us = time_us_32() - t_us;
+        duration_ns = t_us * 1000 / NB_TESTS;
+        multiqore_printf("note_manager.get_audio(...) [Full pool, saw] : %u ns\n", duration_ns);
+
+        // Release notes
         for(unsigned int i = 0; i < NB_ACTIVE_NOTES; i++)
         {
             g_midi_internal_buffer.push(midi_event_note_onoff(MIDI_NOTE_OFF, 0, 12*i, 0x7F));
